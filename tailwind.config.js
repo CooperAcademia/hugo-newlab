29da1084ac083ef3486932b09009e98878872e6f
Gavin Cooper <phd@gavincooper.net>
2023-03-21 14:18:45 +1100

    Improve colour setup, added layouts, some styling
    * New colour setup for tailwindcss
    * Added new layouts for blog, media, people and software sections
    * Updated styling for footer
    * Updated styling for header
    * Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["content/**/*.md", "layouts/**/*.html"],
+  content: ["content/**/*.md", "layouts/**/*.html", "../../content/**/*.md", "../../layouts/**/*.html"],
   theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'white': '#ffffff',
-      }
-    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      primary: colors.purple,
+      secondary: colors.green,
+    }
   },
   plugins: [],
 }
