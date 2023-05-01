f5c5cb584f4ccf806dc5bc8a1b228051dac33040
Gavin Cooper <phd@gavincooper.net>
2023-05-01 16:00:42 +1000

    Update css for better styling, major improvement to people layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,17 @@ const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["content/**/*.md", "layouts/**/*.html", "../../content/**/*.md", "../../layouts/**/*.html"],
+  content: ["layouts/**/*.html", "../../content/**/*.md", "../../layouts/**/*.html"],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      primary: colors.purple,
-      secondary: colors.green,
-    }
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        primary: colors.purple,
+        secondary: colors.green,
+      },
+    },
   },
   plugins: [],
 }
