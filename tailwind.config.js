53b73583365d65284a53227ce03b87e998d2ef7d
Gavin Cooper <phd@gavincooper.net>
2023-08-11 19:12:40 +1000

    Latest styling changes before change to module

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["layouts/**/*.html", "../../content/**/*.md", "../../layouts/**/*.html"],
+  content: ["layouts/**/*.html", "../../content/**/*.md", "../../layouts/**/*.html", "../../hugo_stats.json"],
   theme: {
     extend: {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
-        'white': '#ffffff',
+        'white': '#fffffe',
         primary: colors.purple,
         secondary: colors.green,
       },
